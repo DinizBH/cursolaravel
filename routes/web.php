bfc9822bb2207e841cd8316257cf1b00b8a74106
Giovane <91229782+DinizBH@users.noreply.github.com>
2025-01-08 16:18:17 -0300

    Controllers e Migrations

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-// rota padrão do site
-Route::get('/', [ProdutoController::class, 'index' ])->name('produto.index');
-Route::get('/produto/{id?}',[ProdutoController::class,'show'])->name('produto.show');
-?>
+Route::resource('produtos', ProdutoController::class );
